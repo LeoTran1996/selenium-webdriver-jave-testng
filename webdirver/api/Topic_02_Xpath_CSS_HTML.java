25bf9dce82dc463d88505a34e02e4059e2e27ca7
LeoTran1996 <tranvananhqc@gmail.com>
2021-03-03 09:41:13 +0700

    take element

--- webdirver/api/Topic_02_Xpath_CSS_HTML.java
+++ webdirver/api/Topic_02_Xpath_CSS_HTML.java
@@ -18,16 +18,26 @@ public class Topic_02_Xpath_CSS_HTML {
 		driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
-		driver.get("http://demo.guru99.com/v4/");
+		driver.get("https://alada.vn/tai-khoan/dang-ky.html");
 	}
 
 	@Test
-	public void TC_01_ValidateCurrentUrl() {
+	public void TC_01_Validate() throws InterruptedException {
 		//Hiểu được HTML của 1 element
 		//Thao tác với Đăng Kí Button
-		
+	
 		//Tại sao phải bắt element
 		//Bắt xong phải làm gì/thao tác như thế nào
+		
+		driver.findElement(By.xpath("//form[@id='frmLogin']//button[text()='ĐĂNG KÝ']")).click();
+		Thread.sleep(5000);
+		
+		//Nhập vào textbox họ tên
+		driver.findElement(By.cssSelector("input[id='txtFirstname']")).sendKeys("Automation FC");
+		//Nhập vào textbox password
+		driver.findElement(By.id("txtPassword")).sendKeys("123456789");
+		Thread.sleep(5000);
+		
 	}
 
 
