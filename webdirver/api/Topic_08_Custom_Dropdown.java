b2c335227d8a584e906719ccd0cafb8233c72a5d
LeoTran1996 <tranvananhqc@gmail.com>
2021-03-25 23:40:10 +0700

    Angular Dropdown

--- webdirver/api/Topic_08_Custom_Dropdown.java
+++ webdirver/api/Topic_08_Custom_Dropdown.java
@@ -3,9 +3,12 @@ package api;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import org.junit.validator.PublicClassValidator;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -17,17 +20,19 @@ import org.testng.annotations.Test;
 public class Topic_08_Custom_Dropdown {
 	WebDriver driver;
 	WebDriverWait expilitwait;
+	JavascriptExecutor jsExecutor;
 
 	@BeforeClass
 	public void beforeClass() {
-		driver = new FirefoxDriver();
+		System.setProperty("webdriver.chrome.driver",".\\Browser\\chromedriver.exe" );
+		driver = new ChromeDriver();
+		jsExecutor = (JavascriptExecutor) driver;
 		expilitwait = new WebDriverWait(driver, 30);
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 
 	}
 
-	@Test
 	public void TC_01_JQuery() {
 		driver.get("http://jqueryui.com/resources/demos/selectmenu/default.html");
 		selectItemInCustomDropdown("//span[@id='number-button']", "//ul[@id='number-menu']//div", "18");
@@ -77,8 +82,10 @@ public class Topic_08_Custom_Dropdown {
 		}
 	}
 	
-	@Test
+	
 	public void TC_02_Default_Dropdown() {
+
+
 		driver.get("https://demo.nopcommerce.com/register");
 		
 		selectItemInCustomDropdown("//select[@name='DateOfBirthDay']", "//select[@name='DateOfBirthDay']/option", "15");
@@ -87,7 +94,24 @@ public class Topic_08_Custom_Dropdown {
 		
 	}
 	
-
+	@Test
+	public void TC_03_Angular() {
+		driver.get("https://ej2.syncfusion.com/angular/demos/?_ga=2.262049992.437420821.1575083417-524628264.1575083417#/material/drop-down-list/data-binding");
+		selectItemInCustomDropdown("//ejs-dropdownlist[@id='games']", "//ul[@id='games_options']/li", "Basketball");
+		sleepInSecond(1);
+		Assert.assertEquals(getAngularDropdownSelectedItemText(), "Basketball");
+		selectItemInCustomDropdown("//ejs-dropdownlist[@id='games']", "//ul[@id='games_options']/li", "Football");
+		sleepInSecond(1);
+		Assert.assertEquals(getAngularDropdownSelectedItemText(), "Football");
+		selectItemInCustomDropdown("//ejs-dropdownlist[@id='games']", "//ul[@id='games_options']/li", "Tennis");
+		sleepInSecond(1);
+		Assert.assertEquals(getAngularDropdownSelectedItemText(), "Tennis");
+		
+	}
+	
+	public String getAngularDropdownSelectedItemText() {
+		return (String) jsExecutor.executeScript("return document.querySelector(\"select[name='games']>option\").text");
+	}
 	public void sleepInSecond(long time) {
 		try {
 			Thread.sleep(time * 1000);
