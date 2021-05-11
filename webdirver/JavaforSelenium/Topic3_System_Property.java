050b637ffb54461d35e571e84e572f67757890ee
LeoTran1996 <tranvananhqc@gmail.com>
2021-05-11 22:21:17 +0700

    .

--- webdirver/JavaforSelenium/Topic3_System_Property.java
+++ webdirver/JavaforSelenium/Topic3_System_Property.java
@@ -4,7 +4,10 @@ public class Topic3_System_Property {
 
 	public static void main(String[] args) {
 		String projectlocation = System.getProperty("user.dir");
+		String osName = System.getProperty("os.name");
 		System.out.println(projectlocation);
+		System.out.println(osName);
+		System.out.println(projectlocation + "\\Browser\\chromedriver.exe");
 	}
 
 }
