da9c4a70aee7b880de30f35f5e47076c780d07bf
LeoTran1996 <tranvananhqc@gmail.com>
2021-05-20 23:15:51 +0700

    wait

--- webdirver/api/Topic_17_Webdriver_Wait_Status_Element_PartI.java
+++ webdirver/api/Topic_17_Webdriver_Wait_Status_Element_PartI.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -20,8 +21,8 @@ public class Topic_17_Webdriver_Wait_Status_Element_PartI {
 	@BeforeClass
 	public void beforeClass() {
 		driver = new FirefoxDriver();
-		explicitWait = new WebDriverWait(driver, 30);
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		explicitWait = new WebDriverWait(driver, 10);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		driver.get("http://live.demoguru99.com/index.php/customer/account/login/");
 	}
@@ -47,7 +48,6 @@ public class Topic_17_Webdriver_Wait_Status_Element_PartI {
 		//Verify
 		Assert.assertTrue(driver.findElement(By.xpath("//div[@id='advice-required-entry-email']")).isDisplayed());
 	}
-	
 	@Test
 	public void TC_02_Element_Invisible_Undisplayed_In_DOM() {
 		driver.navigate().refresh();
@@ -56,31 +56,50 @@ public class Topic_17_Webdriver_Wait_Status_Element_PartI {
 		// + Element co trong DOM(Case 1)
 	
 		// Wait My Account link at header is Invisible/Undisplayed
-		
-		explicitWait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//div[@id='advice-required-entry-email']")));
+		explicitWait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//div[@id='header-account']//a[text()='My Account']")));
 		
 	}
-	
 	@Test
 	public void TC_02_Element_Invisible_Undisplayed_Not_In_DOM() {
 		driver.navigate().refresh();
 		// Dieu kien de 1 Element invisible/undisplayed
 		// + Element khong co tren UI(bat buoc)
-		// + Element khong co trong DOM(Case2)
+		// + Element co khong trong DOM(Case2)
 		
 		//Wait Email error message is invisible/undisplayed
-		explicitWait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//div[@id='header-account']//a[text()='My Account']")));
+		explicitWait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//div[@id='advice-required-entry-email']")));
+		
 	}
-	
 	
 
 	@Test
 	public void TC_03_Element_Presence() {
+		 driver.navigate().refresh();
+		// Dieu kien de 1 Element Presence
+		// + Element khong co tren UI(case1)
+		// + Element co tren UI(case2)
+		// + Element co trong DOM(bat buoc)
 		
+		// Wait My Account link at header is presence - Element khong co tren UI(case1)
+		explicitWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@id='header-account']//a[text()='My Account']")));
+		
+		// Wait My Account link at footer is presence - Element co tren UI(case2)
+		explicitWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='footer']//a[text()='My Account']")));
 	}
 	@Test
 	public void TC_04_Element_Staleness() {
+		driver.navigate().refresh();
+		// Dieu kien de 1 Element Presence
+		// + Element khong co tren UI(bat buoc)
+		// + Element khong co trong DOM(bat buoc)
+		// 1- Tim Element luc no dag xuat hien -> luu vao bien
+		driver.findElement(By.xpath("//button[@title='Login']")).click();
+		WebElement elementStaleness = driver.findElement(By.xpath("//div[@id='advice-required-entry-email']"));
+		driver.navigate().refresh();
+		// 2- Element khong con xuat hien nua -> Kiem tra(wait) no Staleness
 		
+		explicitWait.until(ExpectedConditions.stalenessOf(elementStaleness));
+				
 	}
 
 
